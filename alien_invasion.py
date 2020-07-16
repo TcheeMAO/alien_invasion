bf00dea5200f2f079703d087ea2c953c1123b32a
Timothy O <timothy.oei@gmail.com>
2020-07-16 00:48:45 -0400

    Initial Commit

---
+++ alien_invasion.py
@@ -0,0 +1,38 @@
+"""In Alien Invasion, the player controls a ship that appears at
+the bottom center of the screen. The player can move the ship
+right and left using the arrow keys and shoot bullets using the
+spacebar. When the game begins, a fleet of aliens fills the sky
+and moves across and down the screen. The player shoots and
+destroys the aliens. If the player shoots all the aliens, a new fleet
+appears that moves faster than the previous fleet. If any alien hits
+the player’s ship or reaches the bottom of the screen, the player
+loses a ship. If the player loses three ships, the game ends."""
+
+import pygame
+import sys
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+
+def run_game():
+    # Initialize pygame, settings, and screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+    # Make a ship.
+    ship = Ship(screen)
+
+    # Set the background color.
+    bg_color = (230, 230, 230)
+
+    # Start the main loop for the game.
+    while True:
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)
+
+
+run_game()
